f4e3e064a4abf5c7acf3200ad528025c9c6a4770
jamooooong <102500033+jamooooong@users.noreply.github.com>
2025-01-17 17:39:50 +0900

    [ 🎉 ] begin project

--- dist/components/button.d.ts
+++ dist/components/button.d.ts
@@ -1,1 +1,11 @@
-export declare const TestButton: () => import("react/jsx-runtime").JSX.Element;
+export interface ButtonProps {
+    /** Is this the principal call to action on the page? */
+    primary?: boolean;
+    /** How large should the button be? */
+    size?: "small" | "medium" | "large";
+    /** Button contents */
+    label: string;
+    /** Optional click handler */
+    onClick?: () => void;
+}
+export declare const Button: ({ primary, label, size, ...props }: ButtonProps) => import("react/jsx-runtime").JSX.Element;
